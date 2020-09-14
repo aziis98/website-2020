8007bf703e1759f2430a065167ce7e4ec07c4757
aziis98 <antonio.delucreziis@gmail.com>
2020-09-14 20:45:50 +0200

    Added projects and email antispam

--- src/static/js/ux.js
+++ src/static/js/ux.js
@@ -7,3 +7,15 @@ if (location.host !== 'aziis98.com') {
 window.addEventListener('scroll', e => {
     document.body.classList.toggle('not-on-top', window.scrollY > 30)
 });
+
+// Just prevents "text"-scraping bots from seeing the email directly.
+customElements.define('antispam-email', class extends HTMLElement {
+    constructor() {
+        super();
+
+        const EMAIL = 'antonio.delucreziis' + '@' + 'gmail.com';
+        this.innerHTML = `<a href="mailto:${EMAIL}">${EMAIL}</a>`;
+    }
+});
+
+
