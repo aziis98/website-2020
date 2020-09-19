039cf7b7da3b62033f5d68d5b6fbbde2e9220e3b
aziis98 <antonio.delucreziis@gmail.com>
2020-09-19 19:17:12 +0200

    Fixed informations and added unipi link in homepage

--- dist/static/js/ux.js
+++ dist/static/js/ux.js
@@ -1,8 +1,11 @@
 
 if (location.host !== 'aziis98.com') {
-    const $nav_appunti = document.getElementById('nav-unipi');
-    $nav_appunti.style.display = '';
+    document.querySelectorAll('.unipi-only').forEach($el => {
+        $el.style.display = '';
+    });
 }
+
+
 
 window.addEventListener('scroll', e => {
     document.body.classList.toggle('not-on-top', window.scrollY > 30)
